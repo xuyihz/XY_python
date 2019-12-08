042a2845425aac9368c09aad51be79d1612b20ec
Xu Yi <xuyi@ziad.cn>
2019-12-08 23:16:45 +0800

    ex23

---
+++ ex5.py
@@ -0,0 +1,18 @@
+name = 'Xu Yi'
+age = 32 # not a lie
+height = 188 # cm
+weight = 80 # kg
+eyes = 'Brown'
+teeth = 'White'
+hair = 'Black'
+
+print(f"Let's talk about {name}.")
+print(f"He's {height} cm tall.")
+print(f"He's {weight} kg heavy.")
+print("Actually that's not too heavy.")
+print(f"He's got {eyes} eyes and {hair} hair.")
+print(f"His teeth are usually {teeth} depending on the coke.")
+
+# this line is tricky, try to get it exactly right
+total = age + height + weight
+print(f"If I add {age}, {height}, and {weight} I get {total}.")
